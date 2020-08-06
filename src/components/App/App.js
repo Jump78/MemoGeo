9dff531dbd2323b1016b0d5e936937f08feec88f
Jump <teboul.clement@gmail.com>
2020-08-06 13:57:31 +0200

    Fix eslint config

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from '../../assets/logo.svg';
 import './App.css';
+import Map from '../Map/Map.js';
 
 function App() {
   return (
     <div className="App">
+      <Map></Map>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>

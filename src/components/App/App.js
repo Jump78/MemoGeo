6e3b25723eade5a11ddea564aaff2b10a8353897
Jump <teboul.clement@gmail.com>
2020-08-06 14:19:21 +0200

    Add map

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from '../../assets/logo.svg';
 import './App.css';
 import Map from '../Map/Map.js';
 
@@ -7,20 +6,6 @@ function App() {
   return (
     <div className="App">
       <Map></Map>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
